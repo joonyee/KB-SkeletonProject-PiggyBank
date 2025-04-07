90fe57236ea38768972ad3e329dfad68b8878099
eeeunjin <eeeunjin1007@gmail.com>
2025-04-07 14:44:18 +0900

    Update AgeExpenseAnalysis.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import PigIntro from "@/views/PigIntro.vue";
-import Login from "@/views/Login.vue";
-import SignUp from "@/views/SignUp.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import PigIntro from '@/views/PigIntro.vue';
+import Login from '@/views/Login.vue';
+import SignUp from '@/views/SignUp.vue';
+import AgeExpenseAnalysis from '@/views/AgeExpenseAnalysis.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,10 +12,15 @@ const router = createRouter({
       name: 'PigIntro',
       component: PigIntro,
     },
+
     { path: '/login', component: Login },
-    { path: '/signup', component: SignUp }
-
+    { path: '/signup', component: SignUp },
+    {
+      path: '/ageExpenseAnalysis',
+      name: 'AgeExpenseAnalysis',
+      component: AgeExpenseAnalysis,
+    },
   ],
-})
+});
 
-export default router
+export default router;
---
+++ src/views/AgeExpenseAnalysis.vue
@@ -0,0 +1,49 @@
+<script setup>
+import ExpenseChart from '../components/ExpenseChart.vue';
+
+const labels = ['식비', '교통비', '쇼핑', '여가', '주거비'];
+const mySpending = [80000, 15000, 89000, 120000, 450000];
+const averageSpending = [65000, 25000, 45000, 80000, 600000];
+</script>
+
+<template>
+  <div class="age-expense-analysis">
+    <div class="card">
+      <h2 class="title">연령대별 소비 분석</h2>
+      <ExpenseChart
+        title=""
+        :labels="labels"
+        :my-data="mySpending"
+        :avg-data="averageSpending"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import '@/assets/styles/global.css';
+
+.age-expense-analysis {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: 20px;
+  background-color: var(--background-color);
+}
+.card {
+  background-color: var(--background-color);
+  padding: 30px;
+  border-radius: 15px;
+  width: 1200px;
+  height: 600px;
+  margin: auto; /* 수평 중앙 정렬  */
+  border: 2px solid var(--primary-color);
+}
+.title {
+  font: var(--ng-bold-28);
+  color: var(--primary-color);
+  text-align: center;
+  margin-bottom: 10px;
+}
+</style>
