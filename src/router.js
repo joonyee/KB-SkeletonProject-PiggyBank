93f62260e7060438da72679e760a4e8c38492491
eeeunjin <eeeunjin1007@gmail.com>
2025-04-07 14:29:10 +0900

    Update 'global.css'

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import PigIntro from "@/views/PigIntro.vue";
-import Login from "@/views/Login.vue";
-import SignUp from "@/views/SignUp.vue";
+import PigIntro from '@/views/PigIntro.vue';
+import Login from '@/views/Login.vue';
+import SignUp from '@/views/SignUp.vue';
 
 const routes = [
   { path: '/', component: PigIntro },
