5cb25b4d6e9d4fd510cb84fed6a958adb4558883
fanfanduck <young18690@gmail.com>
2025-04-09 11:39:42 +0900

    conflict 해결

---
+++ src/router.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import PigIntro from '@/views/PigIntro.vue';
+import Login from '@/views/Login.vue';
+import SignUp from '@/views/SignUp.vue';
+import AgeExpenseAnalysis from '@/views/AgeExpenseAnalysis.vue';
+import ExpenseList from '@/views/ExpenseList.vue';
+import MonthlyAnalysis from './views/MonthlyAnalysis.vue';
+
+import MyPage from './views/MyPage.vue';
+import Home from '@/views/Home.vue';
+
+
+
+const routes = [
+  { path: '/', component: PigIntro },
+  { path: '/login', component: Login },
+  { path: '/signup', component: SignUp },
+  { path: '/home', component: Home },
+  { path: '/ageExpenseAnalysis', component: AgeExpenseAnalysis },
+  { path: '/expenseList', component: ExpenseList },
+  { path: '/monthlyAnalysis', component: MonthlyAnalysis },
+ 
+  { path: '/myPage', component: MyPage },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
