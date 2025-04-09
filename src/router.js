ef1b0dc5316243b09e3dde811eadd7b8faa51b4b
joonyee <ssugjoon@skuniv.ac.kr>
2025-04-09 13:14:29 +0900

    refactor: 라우터 연결 추가

--- src/router.js
+++ src/router.js
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import PigIntro from '@/views/PigIntro.vue';
-import Login from '@/views/Login.vue';
-import SignUp from '@/views/SignUp.vue';
-import AgeExpenseAnalysis from '@/views/AgeExpenseAnalysis.vue';
-import ExpenseList from '@/views/ExpenseList.vue';
-import MonthlyAnalysis from './views/MonthlyAnalysis.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import PigIntro from "@/views/PigIntro.vue";
+import Login from "@/views/Login.vue";
+import SignUp from "@/views/SignUp.vue";
+import AgeExpenseAnalysis from "@/views/AgeExpenseAnalysis.vue";
+import ExpenseList from "@/views/ExpenseList.vue";
+import MonthlyAnalysis from "./views/MonthlyAnalysis.vue";
+import ExpenseTendency from "@/views/ExpenseTendency.vue";
 
-import Home from '@/views/Home.vue';
-
+import Home from "@/views/Home.vue";
 
 const routes = [
-  { path: '/', component: PigIntro },
-  { path: '/login', component: Login },
-  { path: '/signup', component: SignUp },
-  { path: '/home', component: Home },
-  { path: '/ageExpenseAnalysis', component: AgeExpenseAnalysis },
-  { path: '/expenseList', component: ExpenseList },
-  { path: '/monthlyAnalysis', component: MonthlyAnalysis },
-
+  { path: "/", component: PigIntro },
+  { path: "/login", component: Login },
+  { path: "/signup", component: SignUp },
+  { path: "/home", component: Home },
+  { path: "/ageExpenseAnalysis", component: AgeExpenseAnalysis },
+  { path: "/expenseList", component: ExpenseList },
+  { path: "/monthlyAnalysis", component: MonthlyAnalysis },
+  { path: "/expenseTendency", component: ExpenseTendency },
 ];
 
 const router = createRouter({
